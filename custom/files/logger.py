ea156c01f825a21589bedff2c39644cbb281e8ff
tushardobhal <tushardobhal@gmail.com>
2018-12-02 02:14:18 -0800

    changed logger to rolling appender

--- custom/files/logger.py
+++ custom/files/logger.py
@@ -1,11 +1,13 @@
 import logging
+import logging.handlers as handlers
 
 # create logger
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 # create file handler which logs even debug messages
-fh = logging.FileHandler('gaze_glasses.log')
+# fh = logging.FileHandler('gaze_glasses.log')
+fh = handlers.RotatingFileHandler('gaze_glasses.log', maxBytes=20*1024*1024, backupCount=5)
 fh.setLevel(logging.DEBUG)
 
 # create formatter and add it to the handlers
